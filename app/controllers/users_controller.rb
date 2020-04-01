d1a992a8593891a12c0c4ebaa82af10355541591
fjt-kodai <fjt.kodai@gmail.com>
2020-04-01 22:51:58 +0900

    ユーザー編集機能の追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,10 @@ class UsersController < ApplicationController
 
   def edit
   end
+
+  def update
+    current_user.update(user_params)
+  end
   
   def show
     @organized_events = Event.where('date >= ? and organizer_id = ?', Date.today, current_user.id).limit(5)
@@ -9,6 +13,12 @@ class UsersController < ApplicationController
     @events = Event.where('date >= ?', Date.today).limit(5)
     @events_all = Event.all
     gon.user_id = current_user.id
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :icon, :email, :password)
   end
 
 end
