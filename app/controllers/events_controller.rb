415802b3acfe55a230bdec90495b582958c3b0c1
fjt-kodai <fjt.kodai@gmail.com>
2020-05-06 23:47:22 +0900

    サーバーサイド 修正

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -20,6 +20,7 @@ class EventsController < ApplicationController
     @venue = @event.venue if @event.venue_id
     @participant = Participant.find_by(user_id: current_user&.id, event_id: params[:id])
     @comments = @event.comments
+    @comment = Comment.new
   end
 
   def update
--- app/models/event.rb
+++ app/models/event.rb
@@ -2,7 +2,7 @@ class Event < ApplicationRecord
 
   has_many :participants
   has_many :users, through: :participants
-  has_many :comments, depemdemt: :destroy
+  has_many :comments, dependent: :destroy
   belongs_to :organizer, class_name: 'User', foreign_key: 'organizer_id'
   belongs_to :venue, optional: true
 
