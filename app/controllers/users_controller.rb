8e3c0bcd61778caf4ea3ea0d7f9329ed942c3d56
fjt-kodai <fjt.kodai@gmail.com>
2020-05-08 23:45:15 +0900

    ユーザーコントローラーの編集

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,5 @@
 class UsersController < ApplicationController
 
-  def edit
-  end
-  
   def show
     @organized_events = Event.where('date >= ? and organizer_id = ?', Date.today, current_user.id).limit(5)
     @participating_events = current_user.events.where('date >= ?', Date.today).limit(5)
@@ -11,4 +8,17 @@ class UsersController < ApplicationController
     gon.user_id = current_user.id
   end
 
+  def edit
+  end
+
+  def update
+    @user_before = current_user.dup
+    @user.update(user_params)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :image)
+  end
 end
